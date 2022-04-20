7a243daec81f41b7e2315fb7f1f0f098c1bdac2f
li-deepa <li.deepa02@gmail.com>
2022-04-20 16:56:47 -0700

    added models and registered on admin

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,56 @@
+from sre_constants import CATEGORY
+from sre_parse import CATEGORIES
+from telnetlib import STATUS
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name=models.CharField(max_length=200,null=True)
+    phone=models.CharField(max_length=200,null=True)
+    email=models.CharField(max_length=200,null=True)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+    #name=models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Tag(models.Model):
+    name=models.CharField(max_length=200,null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY=(
+              ('Indoor','Indoor'),
+              ('Out door','Out door'),
+
+    )
+    name=models.CharField(max_length=200,null=True)
+    price=models.FloatField(null=True)
+    description=models.CharField(max_length=200,null=True,blank=True)
+    category=models.CharField(max_length=200,null=True,choices=CATEGORY)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+    tags=models.ManyToManyField(Tag)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+
+class Order(models.Model):
+    STATUS=(
+        ('pending','pending'),
+        ('out for delivery','out for delivery'),
+        ('delivered','delivered')
+    )
+    customer=models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
+    product=models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+    status=models.CharField(max_length=200,null=True,choices=STATUS)
+    
+
+
+    def __str__(self) -> str:
+        return self.status
