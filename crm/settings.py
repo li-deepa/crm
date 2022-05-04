68aeefc864e78484ebe003954da55db1f7f0634f
li-deepa <li.deepa02@gmail.com>
2022-05-04 15:10:13 -0700

    deploying to heroku

--- crm/settings.py
+++ crm/settings.py
@@ -87,22 +87,17 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Demo_test',
+        'USER':'postgres',
+        'PASSWORD':'deepika5',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'Demo_test',
-#         'USER':'postgres',
-#         'PASSWORD':'deepika5',
-#         'HOST':'localhost',
-#         'PORT':'5432'
-#     }
-# }
 
 
 # Password validation
