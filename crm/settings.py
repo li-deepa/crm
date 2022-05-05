075b015b8ad27b21f56b7627923ee11bd43c9089
li-deepa <li.deepa02@gmail.com>
2022-05-05 16:22:46 -0700

    make it better

--- crm/settings.py
+++ crm/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
    
 ]
-STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
+# STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
 MEDIA_ROOT=os.path.join(BASE_DIR, "static/images",)
 
